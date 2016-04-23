2677a0462868f477d816c1814bfd7f4dcbd1b8c1
Jakub <staszewski.jakub@gmail.com>
2016-04-23 21:19:55 +0200

    poprawka funkcji

--- kalkulator.py
+++ kalkulator.py
@@ -7,7 +7,16 @@ def ciag_fibonacciego(n):
 fibonacci = []
 ilosc = int(input())
 
+def new_function:
+	print("rotfl")
+
+def nowa_dzialajaca_funkcja(n):
+	if n>0:
+		print("dodatnia")
+	else:
+		print("niedodatnia")
+	
 for i in range(ilosc):
-    fibonacci.append(ciag_fibonacciego(i))
+    fibonacci.append(ciag_fibonacciego(i*2))
 
 print (fibonacci)
